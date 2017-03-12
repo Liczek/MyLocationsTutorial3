767a708de8c2114156a0e947859975143dbc7e04
Paweł Liczmański <paweliczek@gmail.com>
2017-03-12 18:35:54 +0100

    02 - Tagging Locations Finished - creating new UIView: rounded withn Image with Text (CGRect, CGPoint, CGSize); making delay for closures , import Dispatch s. 129

---
+++ MyLocationTutorial3/MyLocationTutorial3/Functions.swift
@@ -0,0 +1,9 @@
+//
+//  Functions.swift
+//  MyLocationTutorial3
+//
+//  Created by Paweł Liczmański on 12.03.2017.
+//  Copyright © 2017 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
---
+++ MyLocationTutorial3/MyLocationTutorial3/HudView.swift
@@ -0,0 +1,9 @@
+//
+//  HudView.swift
+//  MyLocationTutorial3
+//
+//  Created by Paweł Liczmański on 10.03.2017.
+//  Copyright © 2017 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
