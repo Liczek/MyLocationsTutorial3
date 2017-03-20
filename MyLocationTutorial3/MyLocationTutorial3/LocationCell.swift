a9187ab3e1c04bb817c1bf4953a212811a6b76b3
Paweł Liczmański <paweliczek@gmail.com>
2017-03-20 20:06:02 +0100

    06 - Photo Picker Finished - photo adding / editing , set photo id , save in documents with specific name, deleting photo form documents when row deleting thumbnail in rows - extension for resizing UIImages s.244

--- MyLocationTutorial3/MyLocationTutorial3/LocationCell.swift
+++ MyLocationTutorial3/MyLocationTutorial3/LocationCell.swift
@@ -12,6 +12,7 @@ class LocationCell: UITableViewCell {
     
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     
     func configure(for location: Location) {
@@ -36,6 +37,15 @@ class LocationCell: UITableViewCell {
         } else {
             addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
         }
+        photoImageView.contentMode = .center
+        photoImageView.image = thumbnail(for: location)
+    }
+    
+    func thumbnail(for location: Location) -> UIImage {
+        if location.hasPhoto, let image = location.photoImage {
+            return image.resizedImage(withBounds: CGSize(width: 52, height: 52))
+        }
+        return UIImage()
     }
 
     override func awakeFromNib() {
---
+++ MyLocationTutorial3/MyLocationTutorial3/UIImage+Resize.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+Resize.swift
+//  MyLocationTutorial3
+//
+//  Created by Paweł Liczmański on 20.03.2017.
+//  Copyright © 2017 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    func resizedImage(withBounds bounds: CGSize) -> UIImage {
+        let horizontalRatio = bounds.width / size.width
+        let verticalRatio = bounds.height / size.height
+        let ratio = min(horizontalRatio, verticalRatio)
+        let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, true, 0)
+        draw(in: CGRect(origin: CGPoint.zero, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+}
