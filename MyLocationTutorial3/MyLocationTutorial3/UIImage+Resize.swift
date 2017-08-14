6298d2ec174b063c5f7fec342bd3245b515fa9e0
Paweł Liczmański <paweliczek@gmail.com>
2017-08-15 00:06:58 +0200

    jakies poprawki

--- MyLocationTutorial3/MyLocationTutorial3/UIImage+Resize.swift
+++ MyLocationTutorial3/MyLocationTutorial3/UIImage+Resize.swift
@@ -14,7 +14,7 @@ extension UIImage {
         let horizontalRatio = bounds.width / size.width
         let verticalRatio = bounds.height / size.height
         let ratio = min(horizontalRatio, verticalRatio)
-        let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        let newSize = CGSize(width: size.width * ratio, height: (size.height / 10) * ratio)
         
         UIGraphicsBeginImageContextWithOptions(newSize, true, 0)
         draw(in: CGRect(origin: CGPoint.zero, size: newSize))
